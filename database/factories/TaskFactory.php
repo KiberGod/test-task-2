280d79b28cdaa6968482c9691d01b47f85121ede
KiberGod <stupakov.kirill13@gmail.com>
2024-02-29 11:23:47 +0200

    Added seeds

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'execution_status' => $this->faker->boolean,
+            'user_id' => rand(1, 30),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
