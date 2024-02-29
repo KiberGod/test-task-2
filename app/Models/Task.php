454e5f399982042a9b5396cb058fea6459639dbe
KiberGod <stupakov.kirill13@gmail.com>
2024-02-29 11:51:17 +0200

    Added model links

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'execution_status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
+    }
 }
